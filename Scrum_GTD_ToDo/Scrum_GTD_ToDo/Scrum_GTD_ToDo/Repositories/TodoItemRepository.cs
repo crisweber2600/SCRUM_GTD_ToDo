556dca7c88ae227d94b14779e0d661545079c3c9
Cris Weber <cris.weber2600@gmail.com>
2019-06-16 17:15:22 -0500

    Setup Database

--- Scrum_GTD_ToDo/Scrum_GTD_ToDo/Scrum_GTD_ToDo/Repositories/TodoItemRepository.cs
+++ Scrum_GTD_ToDo/Scrum_GTD_ToDo/Scrum_GTD_ToDo/Repositories/TodoItemRepository.cs
@@ -3,14 +3,31 @@ using System.Collections.Generic;
 using System.Text;
 using Scrum_GTD_ToDo.Models;
 using System.Threading.Tasks;
-
+using SQLite;
+using System.IO;
 namespace Scrum_GTD_ToDo.Repositories
 {
     public class TodoItemRepository : ITodoItemRepository
     {
+        private SQLiteAsyncConnection connection;
         public event EventHandler<TodoItem> OnItemAdded;
         public event EventHandler<TodoItem> OnItemUpdated;
+        private async Task CreateConnection()
+        {
+            if(connection !=null)
+            {
+                return;
+            }
+            var documentPath = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+            var databasePath = Path.Combine(documentPath, "TodoItems.db");
 
+            connection = new SQLiteAsyncConnection(databasePath);
+            await connection.CreateTableAsync<TodoItem>();
+            if(await connection.Table<TodoItem>().CountAsync()==0)
+            {
+                await connection.InsertAsync(new TodoItem() { Title = "Welcome to the best GTD System"})
+            }
+        }
         public async Task AddItem(TodoItem item)
         {
             throw new NotImplementedException();
@@ -39,4 +56,5 @@ namespace Scrum_GTD_ToDo.Repositories
             throw new NotImplementedException();
         }
     }
+
 }
