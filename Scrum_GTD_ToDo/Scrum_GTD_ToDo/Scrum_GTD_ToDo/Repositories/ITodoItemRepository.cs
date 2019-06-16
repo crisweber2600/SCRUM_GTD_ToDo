64e0580dcea28df94d7eec463c12cadb92240d01
Cris Weber <cris.weber2600@gmail.com>
2019-06-16 17:03:36 -0500

    Interface Setup

---
+++ Scrum_GTD_ToDo/Scrum_GTD_ToDo/Scrum_GTD_ToDo/Repositories/ITodoItemRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Scrum_GTD_ToDo.Models;
+
+namespace Scrum_GTD_ToDo.Repositories
+{
+    public interface ITodoItemRepository
+    {
+        event EventHandler<TodoItem> OnItemAdded;
+        event EventHandler<TodoItem> OnItemUpdated;
+        Task<List<TodoItem>> GetItems();
+        Task AddItem(TodoItem item);
+        Task UpdateItem(TodoItem item);
+        Task AddOrUpdate(TodoItem item);
+    }
+}
---
+++ Scrum_GTD_ToDo/Scrum_GTD_ToDo/Scrum_GTD_ToDo/Repositories/TodoItemRepository.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Scrum_GTD_ToDo.Models;
+using System.Threading.Tasks;
+
+namespace Scrum_GTD_ToDo.Repositories
+{
+    public class TodoItemRepository : ITodoItemRepository
+    {
+        public event EventHandler<TodoItem> OnItemAdded;
+        public event EventHandler<TodoItem> OnItemUpdated;
+
+        public async Task AddItem(TodoItem item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task AddOrUpdate(TodoItem item)
+        {
+            if(item.Id == 0)
+            {
+                await AddItem(item);
+            }
+            else
+            {
+                await UpdateItem(item);
+            }
+            throw new NotImplementedException();
+        }
+
+        public async Task<List<TodoItem>> GetItems()
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task UpdateItem(TodoItem item)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
