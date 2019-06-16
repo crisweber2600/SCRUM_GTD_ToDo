840e0741ab0843572681fc37ad2954599e7c3972
Cris Weber <cris.weber2600@gmail.com>
2019-06-16 18:03:14 -0500

    ItemViewModel

---
+++ Scrum_GTD_ToDo/Scrum_GTD_ToDo/Scrum_GTD_ToDo/ViewModel/ItemViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Scrum_GTD_ToDo.Repositories;
+using Scrum_GTD_ToDo.Models;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace Scrum_GTD_ToDo.ViewModel
+{
+    public class ItemViewModel:ViewModel
+    {
+        private TodoItemRepository repository;
+        public ItemViewModel(TodoItemRepository repository)
+        {
+            this.repository = repository;
+        }
+    }
+}
