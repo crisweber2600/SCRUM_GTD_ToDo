7c2705fca62294c95c39539dd74ed5f2ea5c6c28
Cris Weber <cris.weber2600@gmail.com>
2019-06-16 18:45:40 -0500

    ItemView

---
+++ Scrum_GTD_ToDo/Scrum_GTD_ToDo/Scrum_GTD_ToDo/Views/ItemView.xaml.cs
@@ -0,0 +1,23 @@
+﻿using Scrum_GTD_ToDo.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Scrum_GTD_ToDo.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class ItemView : ContentPage
+    {
+        public ItemView(ItemViewModel viewmodel)
+        {
+            InitializeComponent();
+            viewmodel.Navigation = Navigation;
+            BindingContext = viewmodel;
+        }
+    }
+}
