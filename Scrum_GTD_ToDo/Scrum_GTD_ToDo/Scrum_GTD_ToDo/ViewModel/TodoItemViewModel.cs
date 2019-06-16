c61c33d68d71b285a220b51a1f2466665c114a72
Cris Weber <cris.weber2600@gmail.com>
2019-06-16 17:57:38 -0500

    TodoItemViewModel

---
+++ Scrum_GTD_ToDo/Scrum_GTD_ToDo/Scrum_GTD_ToDo/ViewModel/TodoItemViewModel.cs
@@ -0,0 +1,16 @@
+﻿using Scrum_GTD_ToDo.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Scrum_GTD_ToDo.ViewModel
+{
+    public class TodoItemViewModel:ViewModel
+    {
+        public TodoItemViewModel(TodoItem item) => Item = item;
+        public event EventHandler ItemStatusChanged;
+        public TodoItem Item { get; private set; }
+        public string StatusText => Item.Completed ? "Reactiveate" : "Completed"; //Todo: Understand
+
+    }
+}
