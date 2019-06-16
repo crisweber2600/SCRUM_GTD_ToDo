c781e5ba10089d658d812b7405016e4c26814ddb
Cris Weber <cris.weber2600@gmail.com>
2019-06-16 17:48:44 -0500

    MVM Fody

---
+++ Scrum_GTD_ToDo/Scrum_GTD_ToDo/Scrum_GTD_ToDo/ViewModel/MainViewModel.cs
@@ -0,0 +1,22 @@
+﻿using Scrum_GTD_ToDo.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Scrum_GTD_ToDo.ViewModel
+{
+    public class MainViewModel : ViewModel
+    {
+        private readonly TodoItemRepository repository;
+        public MainViewModel(TodoItemRepository repository)
+        {
+            this.repository = repository;
+            Task.Run(async () => await LoadData());
+        }
+        private async Task LoadData()
+        {
+
+        }
+    }
+}
