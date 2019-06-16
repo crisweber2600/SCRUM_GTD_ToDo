a557e3f504ab17033bc0623b7b9c9ac568e11935
Cris Weber <cris.weber2600@gmail.com>
2019-06-16 17:34:11 -0500

    View Model Created

---
+++ Scrum_GTD_ToDo/Scrum_GTD_ToDo/Scrum_GTD_ToDo/ViewModel/ViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.ComponentModel;
+using Xamarin.Forms;
+
+namespace Scrum_GTD_ToDo.ViewModel
+{
+    public abstract class ViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void RaisePropertyChange(params string[] propertyNames)
+        {
+            foreach(var propertyName in propertyNames)
+            {
+                PropertyChanged?.Invoke(this,new PropertyChangedEventArgs(propertyName));
+            }
+        }
+        public INavigation navigation { get; set; }
+    }
+}
